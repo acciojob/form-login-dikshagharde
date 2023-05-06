a8d7f76f896b50faf40fa66bc4624c7643f8b9a4
dikshagharde <117832798+dikshagharde@users.noreply.github.com>
2023-05-06 22:03:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+
+// const form =document.getElementById('form1');
+
 function getFormvalue() {
     //Write your code here
+const fname=document.getElementById("fname").value;
+ const lname=document.getElementById("lname").value;
+    
+console.log(fname+" "+lname);
+alert(fname+" "+lname);
 
 }
